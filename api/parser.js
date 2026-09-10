fac5c09528ad3b76ee6d505895aaf3da324c9289
MugurelG <ubuntufuture@gmail.com>
2026-09-10 23:30:40 +0300

    Fix: Actualizare pachet AI si model gemini-1.5-pro

--- api/parser.js
+++ api/parser.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     }
 
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash-latest' });
+   const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' });
     const { prompt, text } = req.body;
     const fullPrompt = `${prompt}\n\nTEXT BRUT:\n${text}`;
 
